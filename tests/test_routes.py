d5d34094109620e1a56695fbfdb5013556552edb
bblove5210 <1714364572qq@gmail.com>
2025-03-05 19:55:13 +0000

    added test case for update with invalid  header

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -251,3 +251,18 @@ class TestYourResourceService(TestCase):
 
         updated_promotion = response.get_json()
         self.assertEqual(updated_promotion["name"], "promo1")
+
+    def test_update_promotion_invalid_header(self):
+        """It should Update an existing promotion"""
+        # create a promotion to update
+        test_promotion = PromotionFactory()
+        response = self.client.post(BASE_URL, json=test_promotion.serialize())
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+        # update the promotion
+        new_promotion = response.get_json()
+        logging.debug(new_promotion)
+        new_promotion["name"] = "promo1"
+        promotion_id = new_promotion["id"]
+        response = self.client.put(f"{BASE_URL}/{promotion_id}")
+        self.assertEqual(response.status_code, status.HTTP_415_UNSUPPORTED_MEDIA_TYPE)
