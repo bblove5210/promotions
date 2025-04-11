d18808ff559fa17259dfc887d87469b7836c4c43
imlixy <19281282@bjtu.edu.cn>
2025-04-11 22:53:07 +0000

    update features/

--- features/steps/ui_steps.py
+++ features/steps/ui_steps.py
@@ -1,7 +1,5 @@
 from behave import when, then
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import Select, WebDriverWait
-from selenium.webdriver.support import expected_conditions
 
 ID_PREFIX = 'promotion_'
 
@@ -12,6 +10,7 @@ def step_impl(context):
     context.driver.get(context.BASE_URL)
     # Uncomment next line to take a screenshot of the web page
     # context.driver.save_screenshot('home_page.png')
+
 
 @then('I should see "{message}" in the title')
 def step_impl(context, message):
