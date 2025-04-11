5a1a2fde2f04cbf6e3c57ed524ac97bd4eb2b2f7
imlixy <19281282@bjtu.edu.cn>
2025-04-11 22:59:12 +0000

    update features/

--- features/steps/ui_steps.py
+++ features/steps/ui_steps.py
@@ -3,23 +3,21 @@ from selenium.webdriver.common.by import By
 
 ID_PREFIX = 'promotion_'
 
-
 @when('I visit the "Home Page"')
-def step_impl(context):
-    """ Make a call to the base URL """
+def step_visit_home_page(context):
+    """Make a call to the base URL."""
     context.driver.get(context.BASE_URL)
-    # Uncomment next line to take a screenshot of the web page
-    # context.driver.save_screenshot('home_page.png')
 
 
 @then('I should see "{message}" in the title')
-def step_impl(context, message):
-    """ Check the document title for a message """
+def step_check_title_contains(context, message):
+    """Check the document title for a specified message."""
     assert message in context.driver.title, f'"{message}" not in page title: {context.driver.title}'
 
 
 @then('I should not see "{text_string}"')
-def step_impl(context, text_string):
+def step_check_no_error_text(context, text_string):
+    """Ensure that the specified text is not present in the page body."""
     element = context.driver.find_element(By.TAG_NAME, 'body')
     error_msg = "I should not see '%s' in '%s'" % (text_string, element.text)
     assert text_string not in element.text, error_msg
