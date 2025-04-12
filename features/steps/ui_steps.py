f9d1c91d1251893d9ec5393cf5a7a424d6311c20
imlixy <19281282@bjtu.edu.cn>
2025-04-12 18:36:39 +0000

    update features/

--- features/steps/ui_steps.py
+++ features/steps/ui_steps.py
@@ -1,24 +1,30 @@
 from behave import when, then
-from selenium.webdriver.common.by import By
-
-ID_PREFIX = 'promotion_'
+import requests
 
 
-@when('I visit the "Home Page"')
-def step_visit_home_page(context):
-    """Make a call to the base URL."""
-    context.driver.get(context.BASE_URL)
+@when('I send a GET request to the "Home Page"')
+def step_send_get_request(context):
+    context.response =  requests.get(context.BASE_URL)
 
 
-@then('I should see "{message}" in the title')
-def step_check_title_contains(context, message):
-    """Check the document title for a specified message."""
-    assert message in context.driver.title, f'"{message}" not in page title: {context.driver.title}'
+@then('the response status code should be {status_code:d}')
+def step_check_status_code(context, status_code):
+    actual_status = context.response.status_code
+    assert actual_status == status_code, f"Expected status code {status_code}, but got {actual_status}"
 
 
-@then('I should not see "{text_string}"')
-def step_check_no_error_text(context, text_string):
-    """Ensure that the specified text is not present in the page body."""
-    element = context.driver.find_element(By.TAG_NAME, 'body')
-    error_msg = "I should not see '%s' in '%s'" % (text_string, element.text)
-    assert text_string not in element.text, error_msg
+@then('the JSON response should have "name" equal to "{expected_name}"')
+def step_check_json_name(context, expected_name):
+    json_data = context.response.json()
+    actual_name = json_data.get("name")
+    assert actual_name == expected_name, f"Expected name '{expected_name}', but got '{actual_name}'"
+
+
+@then('the JSON response should have "version" equal to "{expected_version}"')
+def step_check_json_version(context, expected_version):
+    """
+    Assert that the JSON response contains the specified 'version' value.
+    """
+    json_data = context.response.json()
+    actual_version = json_data.get("version")
+    assert actual_version == expected_version, f"Expected version '{expected_version}', but got '{actual_version}'"
--- features/ui.feature
+++ features/ui.feature
@@ -1,10 +1,11 @@
 Feature: The promotion service back-end
-    As a Promotion admin
+    As a Promotion developer
     I need a RESTful catalog service
     So that I can keep track of all my promotions
 
 
 Scenario: The server is running
-    When I visit the "Home Page"
-    Then I should see "Promotion REST API Service" in the title
-    And I should not see "404 Not Found"
+    When I send a GET request to the "Home Page"
+    Then the response status code should be 200
+    And the JSON response should have "name" equal to "Promotion REST API Service"
+    And the JSON response should have "version" equal to "1.0"
